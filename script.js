e06f9f1ca5e6461b5b1fe29b9d7b17d88b28e2e8
Webcraft Innovation <135718962+samranrajput@users.noreply.github.com>
2026-04-17 09:33:28 +0500

    Complete Clock

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//////////// Analog Clock /////////////
-
-let clockTime = new Date();
+//////////// Analog & Digital Clock /////////////
 
 let monthList = [
   "January",
@@ -27,121 +25,61 @@ let DayList = [
   "Saturday",
 ];
 
-let clockSecond = clockTime.getSeconds() + 1;
-let secondDegree = clockSecond * 6;
-document.getElementById("second-needle").style.transform =
-  `rotate(${secondDegree}deg)`;
-
-let clockMinute = clockTime.getMinutes();
-let minutDegree = clockMinute * 6 + clockSecond * 0.1;
-document.getElementById("minut-needle").style.transform =
-  `rotate(${minutDegree}deg)`;
-
-let clockHour = clockTime.getHours();
-let hourDegree = clockHour * 30 + clockMinute * 0.5;
-document.getElementById("hour-needle").style.transform =
-  `rotate(${hourDegree}deg)`;
-
-setInterval(function () {
+function updateClock() {
   let clockTime = new Date();
-  let clockSecond = clockTime.getSeconds() + 1;
-  let secondDegree = clockSecond * 6;
+
+  let clockSecond = clockTime.getSeconds();
+  let clockMinute = clockTime.getMinutes();
+  let clockHour = clockTime.getHours();
+
+  let secondDegree = (clockSecond + 1) * 6;
   document.getElementById("second-needle").style.transform =
     `rotate(${secondDegree}deg)`;
 
-  let clockMinute = clockTime.getMinutes();
   let minutDegree = clockMinute * 6 + clockSecond * 0.1;
   document.getElementById("minut-needle").style.transform =
     `rotate(${minutDegree}deg)`;
 
-  let clockHour = clockTime.getHours();
-  let hourDegree = clockHour * 30 + clockMinute * 0.5;
+  let hourDegree = (clockHour % 12) * 30 + clockMinute * 0.5;
   document.getElementById("hour-needle").style.transform =
     `rotate(${hourDegree}deg)`;
-}, 1000);
 
-//////////// Digital Clock /////////////
-
-let creatTime = new Date();
-
-let month = creatTime.getMonth();
-let monthName = monthList[month];
-document.getElementsByClassName("month")[0].innerHTML = monthName;
-document.getElementsByClassName("month")[1].innerHTML = monthName;
-
-let date = creatTime.getDate();
-document.getElementsByClassName("date")[0].innerHTML = date + ",";
-document.getElementsByClassName("date")[1].innerHTML = date + ",";
-
-let year = creatTime.getFullYear();
-document.getElementsByClassName("year")[0].innerHTML = year;
-document.getElementsByClassName("year")[1].innerHTML = year;
-
-let day = creatTime.getDay();
-let dayName = DayList[day];
-document.getElementsByClassName("day")[0].innerHTML = dayName;
-document.getElementsByClassName("day")[1].innerHTML = dayName;
-
-let second = creatTime.getSeconds();
-document.getElementsByClassName("second")[0].innerHTML = second
-  .toString()
-  .padStart(2, "0");
-
-let minute = creatTime.getMinutes();
-document.getElementsByClassName("minute")[0].innerHTML = minute
-  .toString()
-  .padStart(2, "0");
-
-let hour = creatTime.getHours() % 12 || 12;
-document.getElementsByClassName("hour")[0].innerHTML = hour
-  .toString()
-  .padStart(2, "0");
-hour >= 12
-  ? (document.getElementById("ampm").innerHTML = "AM")
-  : (document.getElementById("ampm").innerHTML = "PM");
-
-setInterval(function () {
-  let creatTime = new Date();
-
-  let month = creatTime.getMonth();
+  let month = clockTime.getMonth();
   let monthName = monthList[month];
-  document.getElementsByClassName("month")[0].innerHTML = monthName;
-  document.getElementsByClassName("month")[1].innerHTML = monthName;
-
-  let date = creatTime.getDate();
-  document.getElementsByClassName("date")[0].innerHTML = date + ",";
-  document.getElementsByClassName("date")[1].innerHTML = date + ",";
-
-  let year = creatTime.getFullYear();
-  document.getElementsByClassName("year")[0].innerHTML = year;
-  document.getElementsByClassName("year")[1].innerHTML = year;
-
-  let day = creatTime.getDay();
+  let date = clockTime.getDate();
+  let year = clockTime.getFullYear();
+  let day = clockTime.getDay();
   let dayName = DayList[day];
-  document.getElementsByClassName("day")[0].innerHTML = dayName;
-  document.getElementsByClassName("day")[1].innerHTML = dayName;
 
-  let second = creatTime.getSeconds() + 1;
-  document.getElementsByClassName("second")[0].innerHTML = second
-    .toString()
-    .padStart(2, "0");
-  if (second == 60) {
-    document.getElementsByClassName("second")[0].innerHTML = "00";
+  const months = document.getElementsByClassName("month");
+  const dates = document.getElementsByClassName("date");
+  const years = document.getElementsByClassName("year");
+  const days = document.getElementsByClassName("day");
+
+  for (let i = 0; i < 2; i++) {
+    if (months[i]) months[i].innerHTML = monthName;
+    if (dates[i]) dates[i].innerHTML = date + ",";
+    if (years[i]) years[i].innerHTML = year;
+    if (days[i]) days[i].innerHTML = dayName;
   }
 
-  let minute = creatTime.getMinutes();
-  document.getElementsByClassName("minute")[0].innerHTML = minute
+  document.getElementsByClassName("second")[0].innerHTML = clockSecond
     .toString()
     .padStart(2, "0");
-  if (minute == 60) {
-    document.getElementsByClassName("minute")[0].innerHTML = "00";
-  }
 
-  let hour = creatTime.getHours() % 12 || 12;
-  document.getElementsByClassName("hour")[0].innerHTML = hour
+  document.getElementsByClassName("minute")[0].innerHTML = clockMinute
     .toString()
     .padStart(2, "0");
-  hour >= 12
-    ? (document.getElementById("ampm").innerHTML = "AM")
-    : (document.getElementById("ampm").innerHTML = "PM");
-}, 1000);
+
+  let displayHour = clockTime.getHours() % 12 || 12;
+  document.getElementsByClassName("hour")[0].innerHTML = displayHour
+    .toString()
+    .padStart(2, "0");
+
+  let ampmStatus = clockTime.getHours() >= 12 ? "PM" : "AM";
+  document.getElementById("ampm").innerHTML = ampmStatus;
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
